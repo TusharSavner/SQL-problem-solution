98e2e9fb88284cee3f78aa3043c1ecfbdb61ad51
Tushar savner <savnertushar@gmail.com>
2025-06-01 21:48:52 +0530

    Update 4.sql

--- LearnSQL/hospital.db/easy/4.sql
+++ LearnSQL/hospital.db/easy/4.sql
@@ -1,6 +1,7 @@
 -- 💡 Problem: Retrieve the first name and last name of patients whose weight falls within the range of 100 to 120 (inclusive).
 -- 🟢 Difficulty: Easy.
 -- 🔗 Source Link: [https://www.sql-practice.com/]
+
 SELECT first_name, last_name
 FROM patients
 WHERE weight BETWEEN 100 AND 120; -- 🔎 Filters patients whose weight is between 100 and 120, including both limits.
