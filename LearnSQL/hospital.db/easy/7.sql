f0fc0e73b3c0878c482ceda8a92dbdd15ea8136d
Tushar savner <savnertushar@gmail.com>
2025-06-01 21:56:31 +0530

    Create 7.sql

---
+++ LearnSQL/hospital.db/easy/7.sql
@@ -0,0 +1,7 @@
+-- 💡 Problem: Retrieve the first name, last name, and full province name of each patient instead of an abbreviated code.
+-- 🟡 Difficulty: Medium.
+-- 🔗 Source Link: [https://www.sql-practice.com/]
+
+SELECT p.first_name, p.last_name, pr.province_name
+FROM patients p
+JOIN province_names pr ON p.province_id = pr.province_id; -- 🔎 Links patients table with province_names to get full province name instead of its abbreviation.
