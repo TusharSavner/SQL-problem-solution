4504709c93f4146a13b69b796a9a03eb557e6e26
Tushar savner <savnertushar@gmail.com>
2025-06-01 22:36:44 +0530

    Create 01.sql

---
+++ LearnSQL/hospital.db/medium/01.sql
@@ -0,0 +1,7 @@
+-- 💡 Problem: Retrieve unique birth years from the patients table and sort them in ascending order.
+-- 🟢 Difficulty: Easy.
+-- 🔗 Source Link: [https://www.sql-practice.com/]
+
+SELECT DISTINCT YEAR(birth_date)
+FROM patients
+ORDER BY YEAR(birth_date) ASC; -- 🔎 Extracts unique birth years and arranges them in increasing order.
