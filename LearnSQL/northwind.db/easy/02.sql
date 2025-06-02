be5afdd084790d890a35418717402cc77c4863d8
Tushar savner <savnertushar@gmail.com>
2025-06-02 11:08:53 +0530

    Create 02.sql

---
+++ LearnSQL/northwind.db/easy/02.sql
@@ -0,0 +1,7 @@
+-- 💡 Problem: Retrieve all contact_name, address, and city of customers who are not from Germany, Mexico, or Spain.
+🟢 Difficulty: Easy.
+🔗 Source Link: [https://www.sql-practice.com/]
+
+  SELECT contact_name, address, city
+FROM customers
+WHERE country NOT IN ('Germany', 'Mexico', 'Spain'); -- 🔎 Filters out customers belonging to the specified countries
