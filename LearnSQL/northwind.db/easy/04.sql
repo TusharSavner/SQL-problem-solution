3aa23160822608cc193560813a79163797cb2472
Tushar savner <savnertushar@gmail.com>
2025-06-02 11:10:58 +0530

    Create 04.sql

---
+++ LearnSQL/northwind.db/easy/04.sql
@@ -0,0 +1,7 @@
+-- 💡 Problem: Retrieve employee_id, order_id, customer_id, required_date, and shipped_date from all orders shipped later than the required date.
+🟢 Difficulty: Easy.
+🔗 Source Link: [https://www.sql-practice.com/]
+
+SELECT employee_id, order_id, customer_id, required_date, shipped_date
+FROM orders
+WHERE shipped_date > required_date; -- 🔎 Identifies orders where the shipment was delayed beyond the required date.
