3c7d412421a2a17d1f41df7a044fee66d8107781
Tushar savner <savnertushar@gmail.com>
2025-06-02 11:15:12 +0530

    Create 08.sql

---
+++ LearnSQL/northwind.db/easy/08.sql
@@ -0,0 +1,7 @@
+-- 💡 Problem: Retrieve the first_name, last_name, and hire_date of the most recently hired employee.
+🟢 Difficulty: Easy.
+🔗 Source Link: [https://www.sql-practice.com/]
+  
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date = (SELECT MAX(hire_date) FROM employees); -- 🔎 Finds the most recent hire date and retrieves the corresponding employee details.
