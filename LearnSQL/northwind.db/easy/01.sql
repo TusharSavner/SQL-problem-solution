ea32d5f29d1bfbff7cf99ca28ed7fbb6dbd332e2
Tushar savner <savnertushar@gmail.com>
2025-06-02 11:07:48 +0530

    Create 01.sql

---
+++ LearnSQL/northwind.db/easy/01.sql
@@ -0,0 +1,7 @@
+-- 💡 Problem: Retrieve category_name and description from the categories table, sorted alphabetically by category_name.
+🟢 Difficulty: Easy.
+🔗 Source Link: [https://www.sql-practice.com/]
+
+SELECT category_name, description
+FROM categories
+ORDER BY category_name; -- 🔎 Fetches category details and arranges them in ascending order by name.
