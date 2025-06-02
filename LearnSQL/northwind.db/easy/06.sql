18b2d46f135193fc34ec33017dde13b5350725b4
Tushar savner <savnertushar@gmail.com>
2025-06-02 11:13:07 +0530

    Create 06.sql

---
+++ LearnSQL/northwind.db/easy/06.sql
@@ -0,0 +1,8 @@
+-- 💡 Problem: Retrieve city, company_name, and contact_name for customers whose city name contains the letter 'L', sorted alphabetically by contact_name.
+🟢 Difficulty: Easy.
+🔗 Source Link: [https://www.sql-practice.com/]
+  
+SELECT city, company_name, contact_name
+FROM customers
+WHERE city LIKE '%l%' OR city LIKE '%L%'
+ORDER BY contact_name; -- 🔎 Filters cities that contain the letter 'L' and arranges results by contact_name.
