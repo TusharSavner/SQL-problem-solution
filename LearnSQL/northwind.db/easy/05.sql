1792eda85702417f6a78a0483e0192d61ba75dfe
Tushar savner <savnertushar@gmail.com>
2025-06-02 11:12:06 +0530

    Create 05.sql

---
+++ LearnSQL/northwind.db/easy/05.sql
@@ -0,0 +1,7 @@
+-- 💡 Problem: Retrieve all even-numbered order_id values from the orders table.
+🟢 Difficulty: Easy.
+🔗 Source Link: [https://www.sql-practice.com/]
+  
+SELECT order_id
+FROM orders
+WHERE order_id % 2 = 0; -- 🔎 Filters orders to include only those with even order_id values.
